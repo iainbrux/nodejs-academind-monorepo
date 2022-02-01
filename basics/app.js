5888961d17cee56684dfc93287f17b16eac5c780
iainbrux <me@iainbrux.com>
2022-02-01 22:01:50 +0000

    create first writeFileSync function dealing with chunks of data

--- basics/app.js
+++ basics/app.js
@@ -1,7 +1,9 @@
 const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
   const url = req.url;
+  const method = req.method;
 
   if (url === "/") {
     res.write("<html>");
@@ -15,6 +17,21 @@ const server = http.createServer((req, res) => {
       </body>`
     );
     res.write("</html>");
+    return res.end();
+  }
+
+  if (url === "/message" && method === "POST") {
+    const body = [];
+    req.on("data", (chunk) => {
+      console.log(chunk);
+      body.push(chunk);
+    });
+    req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split("=")[1];
+      fs.writeFileSync("message.txt", message);
+    });
+    res.writeHead(302, { location: "/" });
     return res.end();
   }
 
