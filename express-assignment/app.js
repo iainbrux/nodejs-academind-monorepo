7b2008c9559a3fce2654a27f15c044ed07566164
iainbrux <me@iainbrux.com>
2022-02-02 22:02:52 +0000

    complete final part for assignment

--- express-assignment/app.js
+++ express-assignment/app.js
@@ -2,13 +2,35 @@ const express = require("express");
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log("In the first middleware!");
-  next();
+app.use("/users", (req, res, next) => {
+  res.send(`
+    <html>
+      <head>
+        <title>Users</title>
+      </head>
+      <body>
+        <ul>
+          <li>User 1</li>
+          <li>User 2</li>
+          <li>User 3</li>
+        </ul>
+      </body>
+    </html>
+  `);
 });
 
-app.use((req, res, next) => {
-  console.log("In the second middleware!");
+app.use("/", (req, res, next) => {
+  res.send(`
+    <html>
+      <head>
+        <title>Home Page</title>
+      </head>
+      <body>
+        <h1>Home Page</h1>
+        <p>This is a home page with some content. Lorem ipsum.</p>
+      </body>
+    </html>
+  `);
 });
 
 app.listen(3000);
