86023b0d66c6dcdc4ecf4e11bcee16b69e813c40
iainbrux <me@iainbrux.com>
2022-02-02 22:43:57 +0000

    add filtering for routes sharing common pathname, in this case /admin

--- express-lesson/app.js
+++ express-lesson/app.js
@@ -9,7 +9,21 @@ const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(adminRoutes);
+app.use("/admin", adminRoutes);
 app.use(shopRoutes);
+
+app.use((req, res, next) => {
+  res.status(404).send(`
+    <html>
+      <head>
+        <title>Not Found</title>
+      </head>
+      <body>
+        <h1>Page not found</h1>
+        <p>Oops! It doesn't look like this page exists.</p>
+      </body>
+    </html>
+  `);
+});
 
 app.listen(3000);
