41b48586acb28514196b90f3a538b04566fdaf63
iainbrux <me@iainbrux.com>
2022-02-02 23:07:39 +0000

    introduce serving static html files with the global __dirname variable and path middleware

--- express-lesson/app.js
+++ express-lesson/app.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const bodyParser = require("body-parser");
 
 const express = require("express");
@@ -13,17 +14,7 @@ app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).send(`
-    <html>
-      <head>
-        <title>Not Found</title>
-      </head>
-      <body>
-        <h1>Page not found</h1>
-        <p>Oops! It doesn't look like this page exists.</p>
-      </body>
-    </html>
-  `);
+  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
 });
 
 app.listen(3000);
